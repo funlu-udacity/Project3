ad99767a35cf10976c24bca62111a564407ada81
Unlu <funlu@LAMU02DF31VMD6R.uhc.com>
2022-02-16 23:32:31 -0800

    added live post test

--- live_post.py
+++ live_post.py
@@ -3,26 +3,26 @@ import requests
 import json
 
 data = {
-    "age": 45,
-    "workclass": "Private",
-    "fnlgt": 280464,
-    "education": "Doctorate",
-    "education_num": 10,
-    "marital_status": "Married-civ-spouse",
-    "occupation": "Exec-managerial",
-    "relationship": "Husband",
-    "race": "White",
-    "sex": "Male",
-    "capital_gain": 510,
-    "capital_loss": 0,
-    "hours_per_week": 60,
-    "native_country": "United-States"
+  "age": 42,
+  "workclass": "Private",
+  "fnlgt": 52789,
+  "education": "Masters",
+  "education_num": 17,
+  "marital_status": "Married",
+  "occupation": "Data-Scientist",
+  "relationship": "Husband",
+  "race": "White",
+  "sex": "Male",
+  "capital_gain": 4174,
+  "capital_loss": 0,
+  "hours_per_week": 40,
+  "native_country": "United-States"
 }
 
-response = requests.post('https://project3-census.herokuapp.com/inference', data=json.dumps(data), headers={'content-type': 'application/json'})
+response = requests.post('https://project3-census.herokuapp.com/predict', data=json.dumps(data), headers={'content-type': 'application/json'})
 
-print(response.status_code)
-print(response.json())
+print("Status Code:", response.status_code)
+print("Prediction: ", response.json())
 print("Live test completed")
 
 
